d5d05d81d14383298a97c0ed5de49001b5abc82c
NoemiP94 <m_emi94@hotmail.it>
2024-07-29 11:10:09 +0200

    add gameover

--- flappy1.py
+++ flappy1.py
@@ -17,10 +17,13 @@ tubo_su = pygame.transform.flip(tubo_giu, False, True)
 SCHERMO = pygame.display.set_mode((288, 512))
 # frame per second verrà aggiornata la finestra di gioco
 FPS = 50
+# velocità di avanzamento
+VEL_AVANZ = 3
 
 def disegna_oggetti():
     SCHERMO.blit(sfondo, (0,0))
     SCHERMO.blit(uccello, (uccellox,uccelloy))
+    SCHERMO.blit(base, (basex, 400))
 
 # aggiorna la schermata di gioco e definisce gli FPS
 def aggiorna():
@@ -29,13 +32,36 @@ def aggiorna():
 
 def inizializza():
     global uccellox, uccelloy, uccello_vely
+    global basex
     uccellox, uccelloy = 60, 150
     uccello_vely = 0
+    basex = 0
+
+def hai_perso():
+    SCHERMO.blit(gameover, (50,180))
+    aggiorna()
+    ricominciamo = False
+    # verrà eseguito all'infinito fino a quando ricominciamo non diventa True
+    while not ricominciamo:
+        for event in pygame.event.get():
+            # quando viene premuto SPAZIO
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                # riporta il gioco allo stato iniziale
+                inizializza()
+                # interrompe il ciclo
+                ricominciamo = True
+            # se viene cliccato il tasto di chiusura il gioco si chiude 
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
 inizializza()
 
 ### ciclo infinito principale ###
 while True:
+    # ad ogni ciclo la base viene spostata sempre più a sinistra
+    basex -= VEL_AVANZ
+    # se la posizione della base è minore ad un certo valore lo riporta alla posizione iniziale
+    if basex < -45: basex = 0
     # gravità
     uccello_vely += 1
     uccelloy += uccello_vely
@@ -49,6 +75,9 @@ while True:
         # se viene cliccato il tasto di chiusura il gioco si chiude 
         if event.type == pygame.QUIT:
             pygame.quit()
+    #se l'uccello tocca la base -> gameover
+    if uccelloy > 380:
+        hai_perso()
 
 
     # aggiornamento schermo
